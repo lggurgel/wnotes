b5f4773de39e02b566cd4040b5372bf3874bde8e
lucasggurgel@gmail.com <lucasggurgel@gmail.com>
2022-04-21 22:36:46 -0300

    updated script workflow

--- wnotes.py
+++ wnotes.py
@@ -1,6 +1,9 @@
 import os
 import pickle
+import sys
+import time
 from pprint import pprint
+from datetime import datetime, timedelta, timezone
 
 
 FILENAME = "notes"
@@ -27,8 +30,41 @@ BANNER = """
  ╚████╔╝ ╚██████╔╝██╗██║██╗╚██████╔╝    ╚██╗██████╔╝███████╗   ██║   ██║  ██║██╔╝  
 """
 
+END_MESSAEGE = """
 
-def print_notes():
+ ██████╗ ██████╗ ██████╗ ██╗ ██████╗  █████╗ ██████╗  ██████╗     ██████╗  ██████╗ ██████╗               
+██╔═══██╗██╔══██╗██╔══██╗██║██╔════╝ ██╔══██╗██╔══██╗██╔═══██╗    ██╔══██╗██╔═══██╗██╔══██╗              
+██║   ██║██████╔╝██████╔╝██║██║  ███╗███████║██║  ██║██║   ██║    ██████╔╝██║   ██║██████╔╝              
+██║   ██║██╔══██╗██╔══██╗██║██║   ██║██╔══██║██║  ██║██║   ██║    ██╔═══╝ ██║   ██║██╔══██╗              
+╚██████╔╝██████╔╝██║  ██║██║╚██████╔╝██║  ██║██████╔╝╚██████╔╝    ██║     ╚██████╔╝██║  ██║              
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝     ╚═╝      ╚═════╝ ╚═╝  ╚═╝              
+                                                                                                         
+███████╗██╗   ██╗ █████╗     ███╗   ███╗███████╗███╗   ██╗███████╗ █████╗  ██████╗ ███████╗███╗   ███╗██╗
+██╔════╝██║   ██║██╔══██╗    ████╗ ████║██╔════╝████╗  ██║██╔════╝██╔══██╗██╔════╝ ██╔════╝████╗ ████║██║
+███████╗██║   ██║███████║    ██╔████╔██║█████╗  ██╔██╗ ██║███████╗███████║██║  ███╗█████╗  ██╔████╔██║██║
+╚════██║██║   ██║██╔══██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║╚════██║██╔══██║██║   ██║██╔══╝  ██║╚██╔╝██║╚═╝
+███████║╚██████╔╝██║  ██║    ██║ ╚═╝ ██║███████╗██║ ╚████║███████║██║  ██║╚██████╔╝███████╗██║ ╚═╝ ██║██╗
+╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝
+                                                                                                         
+
+"""
+
+SENTONEL_NAME = "ana"
+SENTINEL_NOTE = "lucas"
+
+
+def get_local_datetime() -> datetime:
+    brazil_timezone = timezone(timedelta(hours=-3))
+    local_datetime = datetime.now().astimezone(brazil_timezone)
+
+    return local_datetime
+
+
+def check_sentinel(input_note: dict):
+    return input_note["name"] == SENTONEL_NAME and input_note["note"] == SENTINEL_NOTE
+
+
+def print_notes() -> None:
     data = []
     with open(FILENAME, "rb") as fr:
         try:
@@ -37,34 +73,55 @@ def print_notes():
         except EOFError:
             pass
 
-    pprint(data)
+    for note in data:
+        print("Nome: {}".format(note["name"]))
+        print("Mensagem: {}".format(note["note"]))
+
+        just_time = note["created_at"].strftime("%H:%M")
+        print("Hora da mensagem: {}".format(just_time))
+        print("\n\n")
 
 
-def read_note():
+def read_note() -> dict:
     input_note = {}
     with open(FILENAME, "ab+") as outfile:
-        name = input("Digite seu nome\n")
-        note = input("\n\nDigite sua mensagem aos noivos\n")
+        name = input("\n\nDigite seu nome:\n")
+        while not name:
+            name = input()
 
-        # if name == "ana" and note == "lucas":
-        #     print_notes()
+        note = input(
+            "\nDigite sua mensagem aos noivos e pressione Enter ↵ para enviar:\n"
+        )
+        while not note:
+            note = input("")
 
-        input_note = {
-            "name": name,
-            "note": note,
-        }
+        input_note = {"name": name, "note": note, "created_at": get_local_datetime()}
 
-        pickle.dump(input_note, outfile)
+        if not check_sentinel(input_note):
+            pickle.dump(input_note, outfile)
 
     return input_note
 
 
-if __name__ == "__main__":
-
+def main_process():
     os.system("clear")
-
     print(BANNER)
 
-    while True:
-        read_note()
+    input_note = read_note()
+
+    if check_sentinel(input_note):
+        os.system("clear")
         print_notes()
+        sys.exit()
+    else:
+
+        print(END_MESSAEGE)
+
+        time.sleep(2)
+        os.system("clear")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    while True:
+        main_process()
